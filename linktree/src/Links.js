5fd5ace941eec47f4223b9b8d066d1219af721d6
Bihefcy <vicarowosafe@gmail.com>
2022-10-28 06:46:52 +0100

    final final

--- linktree/src/Links.js
+++ linktree/src/Links.js
@@ -2,27 +2,15 @@ const Links = () => {
     return ( 
         <div className="links">
             <div className="linka">
-                <a href="https://mobile.twitter.com/clearystal" target="_blank" rel="noreferrer">
-                    <button className="link">
-                    Twitter
-                    </button>
-                    
-                </a>
+                <a href="https://mobile.twitter.com/clearystal" target="_blank" rel="noreferrer" className="link"><p>Twitter</p></a>
             </div>
             
             <div className="linka">
-                <a href="https://training.zuri.team" target="_blank" rel="noreferrer">
-                    <button className="link" id="btn_zuri">
-                    Zuri Team
-                    </button>
-                </a>
+                <a href="https://training.zuri.team" target="_blank" rel="noreferrer" className="link" id="btn__zuri"><p>Zuri Team</p></a>
             </div>
             
             <div className="linka">
-                <a href="https://books.zuri.team" target="_blank" rel="noreferrer">
-                    <button className="link" id="books">
-                    Zuri Books
-                    </button>
+                <a href="https://books.zuri.team" target="_blank" rel="noreferrer" className="link" id="books"><p>Zuri Books</p>
                     <div className="link-text">
                         <p>You can find different books about design and coding by clicking on this link.</p>
                     </div>
@@ -30,10 +18,9 @@ const Links = () => {
             </div>
             
             <div className="linka">
-               <a href="https://books.zuri.team/python-for-beginners?ref_id=Victor" target="_blank" rel="noreferrer">
-                    <button className="link" id="book_python">
-                    Python Books
-                    </button>
+               <a href="https://books.zuri.team/python-for-beginners?ref_id=Victor" target="_blank" rel="noreferrer" className="link" id="book_python">
+                    <p>Python Books</p>
+                    
                     <div className="link-text">
                         <p>Python's extensive uses in web development, complex data analysis, and machine learning drive its popularity. To get started on learning this programmng language, click on this link.</p>
                     </div>
@@ -41,10 +28,9 @@ const Links = () => {
             </div>
             
             <div className="linka">
-                <a href="https://background.zuri.team" target="_blank" rel="noreferrer">
-                    <button className="link" id="pitch">
-                    Background Check for Coders
-                    </button>
+                <a href="https://background.zuri.team" target="_blank" rel="noreferrer" className="link" id="pitch">
+                   <p>Background Check for Coders</p>
+                    
                     <div className="link-text">
                         <p>Job interviews remain the most important part of making a new hire, so therefore you'll need to find out the background of the developer you want to hire. Zuri Background Check offers the best way to accomplish this.</p>
                     </div>
@@ -52,10 +38,9 @@ const Links = () => {
             </div>
             
             <div className="linka">
-                <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer">
-                    <button className="link" id="book_design">
-                    Design Books
-                    </button>
+                <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer" className="link" id="book_design">
+                   <p>Design Books</p>
+                    
                     <div className="link-text">
                         <p>The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified. Remains a fundamental necessity to overcome the hurdles of each challenge meted out.</p>
                     </div>
